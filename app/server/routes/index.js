29b77a2122234ab7eb191f47b4ec94f29ee8e8c2
Gianluca Del Gobbo <g.delgobbo@flyer.it>
2014-04-18 12:40:19 +0200

    Some updates

--- app/server/routes/index.js
+++ app/server/routes/index.js
@@ -7,8 +7,11 @@ var signature = "\n______________________________________\nLPM - Live Performers
 
 
 exports.get = function get(req, res) {
-    res.render('index', { locals: {title:"Menu Manager",post:[],results:[],failed:[[],[]], success:[[],[]] }});
+	res.render('index', { locals: {title:"Menu Manager",post:[],results:[],failed:[[],[]], success:[[],[]] }});
 };
+
+
+
 exports.post = function get(req, res) {
 	// console.log(res);
 	// without auth -- read only
@@ -19,40 +22,51 @@ exports.post = function get(req, res) {
 
 		// set auth to be able to edit/add/delete
 		my_sheet.setAuth(req.body.email,req.body.password, function(err){
-		    my_sheet.getInfo( function( err, sheet_info ){
-		        console.log( sheet_info.title + ' is loaded' );
+			my_sheet.getInfo( function( err, sheet_info ){
+				console.log( sheet_info.title + ' is loaded' );
 
 
-                    // use worksheet object if you want to forget about ids
-                var result = [];
-                sheet_info.worksheets.forEach(function(element, index, array) {
-                        var tmp = {title:element.title,items:[]};
-                        element.getRows( function( err, rows ){
-                            tmp.items = rows;
-                            result.push(tmp)
-                            if (index == array.length-1) res.render('index', { locals: {title:"Menu Manager",post:req.body,results:result}});
-                        });
-                    }
-                )
-		    });
+					// use worksheet object if you want to forget about ids
+				var result = [];
+				sheet_info.worksheets.forEach(function(element, index, array) {
+						var tmp = {title:element.title,items:[]};
+						element.getRows( function( err, rows ){
+							tmp.items = rows;
+							result.push(tmp)
+							if (index == array.length-1) {
+								result.sort(function (a,b) {
+										if (a.last_nom < b.last_nom)
+											return -1;
+										if (a.last_nom > b.last_nom)
+											return 1;
+										return 0;
+									}
+									
+								);
+								res.render('index', { locals: {title:"Menu Manager",post:req.body,results:result}});
+							}
+						});
+					}
+				)
+			});
 		});
 	} else {
-	    res.render('index', { locals: {title:"Menu Manager",post:req.body,results:[],failed:[[],[]], success:[[],[]] }});
+		res.render('index', { locals: {title:"Menu Manager",post:req.body,results:[],failed:[[],[]], success:[[],[]] }});
 	}
 	/*
 	my_sheet.getRows( 1, function(err, row_data){
-	    console.log( err)
-	    console.log( row_data)
-	    console.log( 'pulled in '+row_data + ' rows ')
+		console.log( err)
+		console.log( row_data)
+		console.log( 'pulled in '+row_data + ' rows ')
 	})
-	    // column names are set by google based on the first row of your sheet
-	    my_sheet.addRow( 2, { colname: 'col value'} );
+		// column names are set by google based on the first row of your sheet
+		my_sheet.addRow( 2, { colname: 'col value'} );
 	
-	    my_sheet.getRows( 2, {
-	        start: 0,            // start index
-	        num: 100            // number of rows to pull
-	    }, function(err, row_data){
-	        // do something...
-	    });
+		my_sheet.getRows( 2, {
+			start: 0,			// start index
+			num: 100			// number of rows to pull
+		}, function(err, row_data){
+			// do something...
+		});
 	*/
 };
